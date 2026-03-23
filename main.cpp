814d1e5ed9095c5da2cd26d3b16c76bb86849054
Kokubo Keizo <Kokubo.Keizo@bit-science.com>
2026-03-24 00:49:18 +0900

    feat: 勇者のモードを追加し、勇者の速度を20に変更

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main() {
     string heroName;
     int slimeHP = 10;
-    int heroSpeed = 10; // 勇者の速度
+    int heroMode = 0; // 勇者のモード(0:通常, 1:攻撃, 2:防御)
+    int heroSpeed = 20; // 勇者の速度
     int slimeSpeed = 5; // スライムの速度
 
     cout << "=== Git Quest ===" << endl;
