d8256222ab44a5d40d5363eec8543ed47d5255bd
Kokubo Keizo <Kokubo.Keizo@bit-science.com>
2026-03-24 00:57:56 +0900

    feat: 勇者の通常攻撃によるダメージとスライムの通常攻撃によるダメージのコメントを追加

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
     string heroName;
     int slimeHP = 10;
     int heroMode = 0; // 勇者のモード(0:通常, 1:攻撃, 2:防御)
+    // とりあえず勇者は通常攻撃で20ダメージ、スライムは通常攻撃で5ダメージ
     int heroSpeed = 20; // 勇者の速度
     int slimeSpeed = 5; // スライムの速度
 
